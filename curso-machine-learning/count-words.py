52bd42c6b5fc6bb496e8b44391c6d3c8298c869e
lucasparreiras <lucasacparreiras@gmail.com>
2016-10-26 21:40:08 -0200

    exercício preparatorio python michine learning

---
+++ curso-machine-learning/count-words.py
@@ -0,0 +1,25 @@
+"""Count words."""
+import collections
+
+def count_words(s, n):
+    """Return the n most frequently occuring words in s."""
+    
+    # TODO: Count the number of occurences of each word in s
+    
+    # TODO: Sort the occurences in descending order (alphabetically in case of ties)
+    
+    # TODO: Return the top n words as a list of tuples (<word>, <count>)
+    list = s.split(' ')
+    c = collections.Counter(list)
+    top_n = sorted(c.items(), key=lambda x: (-x[1], x[0]))
+    return top_n[:n]
+
+
+def test_run():
+    """Test count_words() with some inputs."""
+    print count_words("cat bat mat cat bat cat", 3)
+    print count_words("betty bought a bit of butter but the butter was bitter", 3)
+
+
+if __name__ == '__main__':
+    test_run()
